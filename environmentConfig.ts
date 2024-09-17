1c1a6592658a11cfe87cf14c67a323a48febdc4b
FacuGauto <fgauto1992@gmail.com>
2024-09-17 19:06:16 -0300

    Ahora

---
+++ environmentConfig.ts
@@ -0,0 +1,4 @@
+export const enviroment =
+{
+  "projectId":"saladejuegos-426ef","appId":"1:330276154267:web:5a7869db2ea36763180f3e","storageBucket":"saladejuegos-426ef.appspot.com","apiKey":"AIzaSyAz34gS-b02-R_g0cZLPvmWvB9IYUJQ4Yg","authDomain":"saladejuegos-426ef.firebaseapp.com","messagingSenderId":"330276154267"
+}
