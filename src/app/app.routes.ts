b43587e919acb3a92845cd42ab6cbbcd8895e7da
FacuGauto <fgauto1992@gmail.com>
2024-09-17 18:56:18 -0300

    Fix firebase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,9 @@ import { LoginComponent } from './componentes/login/login.component';
 import { PagenotfoundComponent } from './componentes/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: "full" },
+    { path: '', redirectTo: '/login', pathMatch: "full" },
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'about', component: QuiensoyComponent },
-    // La ruta comodin debe ir siempre al final
     { path: '**', component: PagenotfoundComponent }
 ];
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
+import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  login: boolean = false;
+  //login: boolean = false;
+  persona = { email: '', password: ''}
+  public user: string = "";
+  public nombre: string = "";
+  public email: string = "";
 
-  validarUsuario() {
-    this.login = true;
-   }
+  constructor(private firestore: Firestore){
+  }
+
+  login(){
+    let  col = collection(this.firestore, 'logins')
+    addDoc(col, {fecha: new Date(), "person": this.persona})
+  }
+
+  //validarUsuario() {
+  //  this.login = true;
+  // }
+
+  autocompletar() {
+    this.persona.email = 'messi@gmail.com';
+    this.persona.password = 'lionel';
+    console.log(this.persona.email);
+    console.log(this.persona.password);
+  }
 
 }
